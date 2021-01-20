edf10e8341303684b120975224594a91509c614e
caymanbarber <barbercayman@gmail.com>
2021-01-20 15:32:06 -0800

    Added ability to communicate with car over PWM and get information from encoder

---
+++ src/sensor.py
@@ -0,0 +1,57 @@
+#/usr/bin/python3
+
+import Jetson.GPIO as GPIO
+from time import time
+
+#NOTE: channel indicates GPIO port
+
+class GPIO_Interaction():
+    def __init__(self, enc_ch, servo_ch, motor_ch):
+        self.enc_ch = enc_ch
+        self.servo_ch = servo_ch
+        self.motor_ch = motor_ch
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.enc_ch, GPIO.IN)
+        GPIO.setup((self.servo_ch, self.motor_ch),GPIO.OUT)
+        self.servo_pwm = GPIO.PWM(self.servo_ch, 50)
+        self.motor_pwm = GPIO.PWM(self.motor_ch, 50)
+
+    def shut_down(self):
+        self.pwm.stop()
+        GPIO.cleanup()
+
+    def format(self, val): #-10 => 8.5 10=>5.5
+        if val < -10:
+            return 8.5
+        elif val > 10:
+            return 5.5
+        else:
+            return val*3/20 + 7
+
+    def set_servo_pwm(self, value):     #-10->10
+        self.servo_pwm.ChangeDutyCycle(self.format(float(value)))
+
+    def set_motor_pwm(self, value):     #-10->10
+        self.motor_pwm.ChangeDutyCycle(self.format(float(value)))
+
+
+class Encoder():
+    def __init__(self, channel, mag_num=2, tire_r=0.035):
+        self.mag_num = mag_num
+        self.channel = channel
+        self.r = tire_r
+        self.tally = 0
+        self.last_time = time.time()
+        GPIO.add_event_detect(self.channel, GPIO.EITHER,callback=self.on_edge)
+
+    def on_edge(self):
+        self.tally += 1
+
+    def get_speed(self):
+        now = time.time()
+        rps = self.tally/(2*self.mag_num*(self.last_time-now))
+        speed = 2*3.1415*rps*self.r
+        self.tally = 0
+        self.last_time = now
+        return speed
