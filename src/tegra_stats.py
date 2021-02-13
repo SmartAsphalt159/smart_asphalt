c1138027671f86da2a94d73de813c65253674694
akettle44 <andrewjkettle@gmail.com>
2021-02-12 17:30:04 -0800

    Able to create dataframe

--- src/tegra_stats.py
+++ src/tegra_stats.py
@@ -2,21 +2,24 @@
 
 """ 
 Script for monitoring the values of a script and logging them for viewing later 
+The output from tegrastats is synchronous (~ every 1s), so the absolute timestamps are not taken. 
+Instead, any plots assume contingous samples, meaning the index can be used for graphing"
 """
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import time
+import re
 import sys
-import timing
-import threading
 import subprocess
 
 class tegra_stats:
 
+	reg = re.compile("POM_5V_IN \d+\/\d+")
 	orig_stdout = sys.stdout
-	timeout 
+	timeout = 0 
 
 	@staticmethod
 	def init_stdout_to_file():
@@ -37,17 +40,30 @@ class tegra_stats:
 	def read_file():
 		#open text file
 		f = open("../logs/rawstats.txt", 'r')
-		lines = f.readlines()
+		line = str(f.readline())
+		count = 0
 		#create dataframe
-		for l in lines:
-			#regex command 
+		df = pd.DataFrame(columns = ["inst_mw", "avg_mw"])
+		while line:
+			mtc = tegra_stats.reg.search(line)
+			if(mtc):
+				raw_match = mtc.group(0)
+				nums = raw_match.split()[1]
+				power = nums.split('/')
+				df.loc[count] = [power[0], power[1]]
+			#else, raise exception
+			line = str(f.readline())
+			count += 1
 
 		#return dataframe
+		return df
 
 def main():
 	proc = tegra_stats.get_tegra_stats()
-	timing.sleep_for(5)
+	time.sleep(5)
 	tegra_stats.kill_tegra_stats(proc)
+	nice = tegra_stats.read_file()
+	print(nice.head())
 
 if __name__ == "__main__":
 	main()
