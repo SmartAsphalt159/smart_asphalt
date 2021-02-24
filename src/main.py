96440b9e422e12b2da9e9f3690b8db42b89f3a13
akettle44 <andrewjkettle@gmail.com>
2021-02-23 16:22:46 -0800

    Updates in main after code review

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ def main():
         exit(0)
     else:
         #type of control
-        c_type = argv[1]
+        c_type = sys.argv[1]
 
     #INIT LOGGER
     log = Sys_logger("Application")
@@ -47,7 +47,7 @@ def main():
     sendport = 6202
     timeout = 2 #seconds
     net_thread_timeout = 5
-    sn = send_network(sendport)
+    sn = network.send_network(sendport)
 
     #GPIO vars
     motor_ch = 32
@@ -63,6 +63,8 @@ def main():
     #LIDAR VARS
     lid_timeout = 10
     lid_thread_timeout = 5
+    #TODO Channel is needed for lidar producer, update to what it is expected to be
+    lidar_channel = 7 #PLACEHOLDER
 
     #CONTROL VARS
     #Velocity constants
@@ -78,16 +80,24 @@ def main():
     #INIT PRODCUER CONSUMERS
 
     #Network
-    network_producer(net_q, net_lock, recvport, log, timeout)
-    network_consumer(net_q, None, net_lock, log, net_thread_timeout)
+    np = network_producer(net_q, net_lock, recvport, log, timeout)
+    nc = network_consumer(net_q, None, net_lock, log, net_thread_timeout)
 
     #Encoder
-    encoder_producer(encoder_q, enc_lock, enc_channel, log, enc_timeout, sample_wait)
-    encoder_consumer(encoder_q, None, enc_lock, log, enc_thread_timeout)
+    ep = encoder_producer(encoder_q, enc_lock, enc_channel, log, enc_timeout, sample_wait)
+    ec = encoder_consumer(encoder_q, None, enc_lock, log, enc_thread_timeout)
 
     #Lidar (pull controls updates)
-    lidar_producer(lidar_q, lid_lock, log, lid_timeout)
-    lidar_consumer(lidar_q, None, lid_lock, log, lid_thread_timeout)
+    lp = lidar_producer(lidar_q, lid_lock, lidar_channel, log, lid_timeout)
+    lc = lidar_consumer(lidar_q, None, lid_lock, log, lid_thread_timeout)
+
+    #start the producer consumer threads
+    np.start()
+    nc.start()
+    ep.start()
+    ec.start()
+    lp.start()
+    lc.start()
 
     try:
         #update local objects (done by threads)
@@ -105,30 +115,45 @@ def main():
 
             while True:
                 controller.control_loop()
-        else if(c_type == "smart"):
+                #Broadcast after control system
+                sn.broadcast_data() #TODO: Cayman the local control system vars need to be parameters here
+        #Uncomment when written
+        #elif(c_type == "smart"):
             #call smart control system
             #TODO: once it is written
 
-        else if(c_type == "lidar"):
+        elif(c_type == "lidar"):
             #call lidar control system
             new_lidar = Lidar()
             gpio = GPIO_Interaction(enc_channel, servo_ch, motor_ch)
-            controller = Lidar_Controls(vp, vi, vd, vk, so, si, sd, new_lidar, gpio)
+            controller = Lidar_Controls(vp, vi, vd, vk, sp, si, sd, new_lidar, gpio)
 
             while True:
                 controller.control_loop()
+                #Broadcast after control system
+                sn.broadcast_data() #TODO: Cayman the local control system vars need to be parameters here
         else:
             log.log_error("Input was not a valid type")
-    except Exception as e:
-        log.log_error("Exitted loop - Exception: ", e)
+    except Exception as e:      
+        err = "Exitted loop - Exception: " + e
+        log.log_error(err)
 
-    #Broadcast after control system
-    sn.broadcast_data() #TODO: Cayman the local control system vars need to be parameters here
 
-    #gracefully exit program
+    #exited from loop
+
+    #halt other threads, they should exit naturally
+    np.halt_thread()
+    nc.halt_thread()
+    ep.halt_thread()
+    ec.halt_thread()
+    lp.halt_thread()
+    lc.halt_thread()
+
+    #gracefully exit program and reset vars
     graceful_shutdown(log)
 
 def graceful_shutdown(log):
+    #TODO: Cayman, how do I use this function, is it initalized somewhere
     GPIO_Interaction.shut_down()
     log.log_info("Shutting down gracefully")
 
