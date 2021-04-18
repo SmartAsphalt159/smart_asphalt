b9c28508240b93abc355773c368383515854652e
adrianosorio <adrianosorio101@gmail.com>
2021-04-17 21:36:33 -0700

    Creation of setup.py for package initialization
    Created a simple skeleton for a setup.py file for packages that we create within our project. Currently just has a basic outline of the package and executes find_packages() to look for pakcages in the project. On a seperate note, the files generated from executing the command "python3 setup.py develop" will be ignored and not committed to the repo.

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smartasphalt',
+    version='1.0',
+    author='Adrian Osorio, Cayman Barber, Andrew Kettle',
+    author_email='adrianosorio101@gmail.com',
+    packages=find_packages(),
+
+)
