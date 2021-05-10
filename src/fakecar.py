fe1a30bd06676545b34b2d8e0a559d4e23d893b0
adrianosorio <adrianosorio101@gmail.com>
2021-05-09 18:32:58 -0700

    Changed Units and Changed the Fake Car
    Updated the velocity controller and encoder to read meters per second not millimeters per second. Also added integration functions from FakeCar and refactored some items in FakeCar.

--- src/fakecar.py
+++ src/fakecar.py
@@ -6,34 +6,35 @@ import matplotlib.pyplot as plt
 from network import recv_network, send_network
 from logger import Sys_logger, Data_logger
 
-class fake_car:
 
-	#fake car constructor
+class FakeCar:
+
+	# fake car constructor
 	def __init__(self, velocity, steering):
 		self.velocity = velocity 
 		self.controller = NetworkAdaptiveCruiseController(is_sim_car=True)
-		self.controller.set_P_value(.5)
-		self.controller.set_I_value(0.4)
+		self.controller.set_proportional_value(.5)
+		self.controller.set_integral_value(0.4)
 		self.desired_velocity = 0
 		self.steering = steering
-		self.input = [] #list of what should happen at each timestep
-		self.error = 0 # starts with 0 error
-		self.step = 0 # amount of runs of controller
-		self.df = pd.DataFrame(columns = ["step", "error", "desired", "actual"])
+		self.input = []  # list of what should happen at each timestep
+		self.error = 0  # starts with 0 error
+		self.step = 0  # amount of runs of controller
+		self.df = pd.DataFrame(columns=["step", "error", "desired", "actual"])
 		self.get_inputs()
 		
-	#append the inputs
+	# append the inputs
 	def get_inputs(self):
-		if(len(sys.argv) < 2):
+		if len(sys.argv) < 2:
 			print("Must input at least one number to test")
 			exit()
 		else:
 			for i in range(1, len(sys.argv)):
 				self.input.append(int(sys.argv[i]))
 
-	#iterate the controller once
+	# iterate the controller once
 	def run_controller(self):
-		#complete once interface is known
+		# complete once interface is known
 		self.desired_velocity = self.input[self.step]
 		self.velocity = self.controller.control_loop(self.velocity, self.desired_velocity)
 		self.error = self.velocity - self.desired_velocity
@@ -59,7 +60,7 @@ class fake_car:
 		#plt.savefig("err_plot.png")
 
 #running fake car
-fk = fake_car(0, 0)
+fk = FakeCar(0, 0)
 while fk.step < (len(sys.argv) - 1):
 	fk.run_controller()
 fk.plotVel()
